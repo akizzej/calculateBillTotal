754db4e17759f80974841d38cf86f80c92a0593e
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-07-02 23:30:07 -0300

    se declara funcion calculateBillTotal, que para 20 debe dar 24,9, funcion ok

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ Suponga que el impuesto de ventas es 9.5% y la propina es 15%.
 NO incline el impuesto sobre las ventas, solo sobre la cantidad de propina.
 var output = calculateBillTotal(20);
 console.log(output); // -> 24,9 */
+function calculateBillTotal(preTaxAndTipAmount) {
+    //Suponga que el impuesto de ventas es 9.5% y la propina es 15%.
+    let tip = (preTaxAndTipAmount/100)*15;
+      let taxes = (preTaxAndTipAmount/100)*9.5;
+      let total = (preTaxAndTipAmount+tip+taxes);
+      return total;
+  }
+  console.log(calculateBillTotal(20));//-> 24,9 
+  
